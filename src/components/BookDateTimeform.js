c7537f4cb72c0264422df116bbf4fdd29406048f
woodelin-florveus <woodelinflorveus@gmail.com>
2021-07-01 19:08:46 -0400

    multistep form added

--- src/components/BookDateTimeform.js
+++ src/components/BookDateTimeform.js
@@ -5,18 +5,32 @@ import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 function BookDateTimeForm(){
 
 
- const handledSchedule = dateTime => {
-        // const [hours, minutes, seconds] = dateTime.split(':')
-        //     console.log ('scheduled : ', `${(hours > 12) ? hours - 12 : hours} : ${minutes}${seconds ? `:${seconds}` : ''}
-        //     ${(hours >= 12) ? 'PM' : 'AM'}`)
+//  const handledSchedule = dateTime => {
+//         // const [hours, minutes, seconds] = dateTime.split(':')
+//         //     console.log ('scheduled : ', `${(hours > 12) ? hours - 12 : hours} : ${minutes}${seconds ? `:${seconds}` : ''}
+//         //     ${(hours >= 12) ? 'PM' : 'AM'}`)
  
-            console.log('scheduled :', dateTime)
-        }
+//             console.log('scheduled :', dateTime)
+//         }
 
-        const testing = () => {
-            console.log("you have been schedu")
+    const timeSlotValidator = slotTime => {
+
+        const eveningTime = new Date(
+            slotTime.getFullYear(),
+            slotTime.getMonth(),
+            slotTime.getDate(),
+            18, 
+            0,
+            0
+        )
+
+        // const isValid = slotTime.getTime() > eveningTime.getTime();
+        // return isValid
+    }
+
+        const chooseTrainer = () => {
+            console.log('Choose Trainer')
         }
- 
 
   return (
       <>
@@ -24,8 +38,8 @@ function BookDateTimeForm(){
 
      <DayTimePicker timeSlotSizeMinutes={15} 
 
-        onConfirm={handledSchedule}
-     
+        onConfirm={chooseTrainer}    
+
      />
 
         </>
