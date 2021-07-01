e4cfe3140f6e3d8c06a0cc5aead645437a7ea189
woodelin-florveus <woodelinflorveus@gmail.com>
2021-06-30 20:49:32 -0400

    add multipage form

--- src/components/BookDateTimeform.js
+++ src/components/BookDateTimeform.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import { render } from 'react-dom';
 import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 
 
 function BookDateTimeForm(){
 
+
+ 
  
 
   return (
       <>
       <h1>Book a Date</h1>
-    
-      {/* <ReactTimeslotCalendar
-    initialDate = { moment([2017, 3, 24]).format() }
-    timeslots = { [
-        ['1', '2'], // 1:00 AM - 2:00 AM
-        ['2', '3'], // 2:00 AM - 3:00 AM
-        ['4', '6'], // 4:00 AM - 6:00 AM
-        '5', // 5:00 AM
-        ['4', '6', '7', '8'], // 4:00 AM - 6:00 AM - 7:00AM - 8:00AM
-    ]}
-    /> */}
 
-     <DayTimePicker timeSlotSizeMinutes={15} />;
+     <DayTimePicker timeSlotSizeMinutes={15} />
 
         </>
     )
