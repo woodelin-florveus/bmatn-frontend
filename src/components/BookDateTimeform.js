c90c43ffc1f43926a0118df81b3d013e558cf1c9
woodelin-florveus <woodelinflorveus@gmail.com>
2021-07-01 15:27:14 -0400

    testing date time conversion

--- src/components/BookDateTimeform.js
+++ src/components/BookDateTimeform.js
@@ -5,14 +5,28 @@ import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 function BookDateTimeForm(){
 
 
+ const handledSchedule = dateTime => {
+        // const [hours, minutes, seconds] = dateTime.split(':')
+        //     console.log ('scheduled : ', `${(hours > 12) ? hours - 12 : hours} : ${minutes}${seconds ? `:${seconds}` : ''}
+        //     ${(hours >= 12) ? 'PM' : 'AM'}`)
  
+            console.log('scheduled :', dateTime)
+        }
+
+        const testing = () => {
+            console.log("you have been schedu")
+        }
  
 
   return (
       <>
       <h1>Book a Date</h1>
 
-     <DayTimePicker timeSlotSizeMinutes={15} />
+     <DayTimePicker timeSlotSizeMinutes={15} 
+
+        onConfirm={handledSchedule}
+     
+     />
 
         </>
     )
