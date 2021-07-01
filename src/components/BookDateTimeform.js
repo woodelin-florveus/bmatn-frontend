3e737a0ba0b8e20f994f1165dbfaf200d9e1ba10
woodelin-florveus <woodelinflorveus@gmail.com>
2021-06-30 20:12:00 -0400

    new Calender

--- src/components/BookDateTimeform.js
+++ src/components/BookDateTimeform.js
@@ -1,24 +1,28 @@
-import moment from 'moment';
-import ReactTimeslotCalendar from 'react-timeslot-calendar-k';
+import React from 'react';
+import { render } from 'react-dom';
+import DayTimePicker from '@mooncake-dev/react-day-time-picker';
 
 
 function BookDateTimeForm(){
 
-
-
+ 
 
   return (
       <>
       <h1>Book a Date</h1>
     
-      <ReactTimeslotCalendar
+      {/* <ReactTimeslotCalendar
     initialDate = { moment([2017, 3, 24]).format() }
     timeslots = { [
-        ['9', '10'],
-        ['10', '11'],
-        ['18'],
-    ] }
-/>
+        ['1', '2'], // 1:00 AM - 2:00 AM
+        ['2', '3'], // 2:00 AM - 3:00 AM
+        ['4', '6'], // 4:00 AM - 6:00 AM
+        '5', // 5:00 AM
+        ['4', '6', '7', '8'], // 4:00 AM - 6:00 AM - 7:00AM - 8:00AM
+    ]}
+    /> */}
+
+     <DayTimePicker timeSlotSizeMinutes={15} />;
 
         </>
     )
