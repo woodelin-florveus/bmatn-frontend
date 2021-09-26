17ddb403c2d2103c0b55e1c65468b83fd8db50ed
woodelin-florveus <woodelinflorveus@gmail.com>
2021-09-26 14:50:56 -0400

    flex items fixed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,31 +1,31 @@
 import React from "react"
-import { Grid, Image } from 'semantic-ui-react'
+
 
 
 
 function Homepage(){
     return (
         
-		<>
+	
 		<section className="intro">
 			<h2 className="section__title section__title--services"> Services We Offer</h2>
 				<div className="services">
 					
-					<div className="service">
+					<div className="service_item">
 						<img src="https://dummyimage.com/300x300/000/fff" alt="service__image" className="service__image" />
 						<h3 className="service__subtitle">Weight Lifting Session</h3>
 						<p>In publishing and graphic design, Lorem ipsum is a placeholder 
 						text commonly used to demonstrate the visual form of a document</p>
 					</div>
 
-					<div className="service">
+					<div className="service_item">
 						<img src="https://dummyimage.com/300x300/000/fff" alt="service__image" className="service__image" />
 						<h3 className="service__subtitle">Weight Lifting Session</h3>
 						<p>In publishing and graphic design, Lorem ipsum is a placeholder 
 						text commonly used to demonstrate the visual form of a document</p>
 					</div>
 
-					<div className="service">
+					<div className="service_item">
 						<img src="https://dummyimage.com/300x300/000/fff" alt="service__image" className="service__image" />
 						<h3 className="service__subtitle">Weight Lifting Session</h3>
 						<p>In publishing and graphic design, Lorem ipsum is a placeholder 
@@ -34,7 +34,7 @@ function Homepage(){
 
 				</div>
 		</section>
-		</>
+	
     )
 }
 
